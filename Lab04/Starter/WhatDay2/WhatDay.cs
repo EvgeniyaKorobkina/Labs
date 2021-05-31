c209b5c67e4b9f0376a566910db290479a9fac2f
EvgeniyaKorobkina <82904333+EvgeniyaKorobkina@users.noreply.github.com>
2021-05-31 23:07:53 +0300

    Update WhatDay.cs

--- Lab04/Starter/WhatDay2/WhatDay.cs
+++ Lab04/Starter/WhatDay2/WhatDay.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System;
 
-namespace WhatDay2
-{
-    class WhatDay
-    {
-        enum MonthName
+enum MonthName
         {
             January,
             February,
@@ -23,34 +16,44 @@ namespace WhatDay2
             December
         }
 
-        static void Main(string[] args)
+
+    class WhatDay
+    {
+        
+
+        static void Main( )
+    { try     
         {
-            Console.Write("Please input a day number between 1 and 365: ");
-            
-            int dayNum = int.Parse(Console.ReadLine());
+            Console.Write("Please enter a day number between 1 and 365: ");
+            string line = Console.ReadLine( );
+            int dayNum = int.Parse(line);
 
-            int monthNum = 0;
+            if (dayNum < 1 || dayNum > 365) {
+            throw new ArgumentOutOfRangeException("Day out of range");
+         }
 
-            foreach (int daysInMonth in DaysInMonths)
-            {
-                if (dayNum <= daysInMonth)
-                {
-                    break;
-                }
-                else
-                {
-                    dayNum -= daysInMonth;
-                    monthNum++;
-                }
-            }
+        int monthNum = 0;
 
-            MonthName temp = (MonthName)monthNum;
-            string monthName = temp.ToString();
+        foreach (int daysInMonth in DaysInMonths) {
+            if (dayNum <= daysInMonth) {
+                break;
+        } else {
+                dayNum -= daysInMonth;
+                monthNum++;
+    }
+}
+MonthName temp = (MonthName)monthNum;
+string monthName = temp.ToString( );
 
-            Console.WriteLine("{0} {1}", dayNum, monthName);
+Console.WriteLine("{0} {1}", dayNum, monthName);
+}
+
+        catch (Exception caught) {
+        Console.WriteLine(caught);
+}
         }
-        // Don't modify anything below here
+        
         static System.Collections.ICollection DaysInMonths
             = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
     }
-}
+
