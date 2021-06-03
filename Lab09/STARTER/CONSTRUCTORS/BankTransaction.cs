8402c312c1b0b99d579db1547adf5b0bafeebd6b
EvgeniyaKorobkina <82904333+EvgeniyaKorobkina@users.noreply.github.com>
2021-06-03 21:46:48 +0300

    Add files via upload

---
+++ Lab09/STARTER/CONSTRUCTORS/BankTransaction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+class BankTransaction
+{
+    private readonly decimal amount;
+    private readonly DateTime when;
+
+    public decimal Amount()
+    {
+        return amount;
+    }
+
+    public DateTime When()
+    {
+        return when;
+    }
+    public BankTransaction(decimal tranAmount)
+    {
+        amount = tranAmount;
+        when = DateTime.Now;
+    }
+
+    public decimal Deposit(decimal amount)
+    {
+        accBal += amount;
+        BankTransaction tran = new BankTransaction(amount);
+        tranQueue.Enqueue(tran);
+        return accBal;
+    }
+
+    public bool Withdraw(decimal amount)
+    {
+        bool sufficientFunds = accBal >= amount;
+        if (sufficientFunds)
+        {
+            accBal -= amount;
+            BankTransaction tran = new BankTransaction(-amount);
+            tranQueue.Enqueue(tran);
+        }
+        return sufficientFunds;
+    }
+
+
+    private Queue tranQueue = new Queue();
+}
